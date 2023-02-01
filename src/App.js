5d3c94289bf3f7dd4ef1708007b01a747b41f4ca
dua <duaaalghassani99@gmail.com>
2023-02-01 13:44:29 +0400

    hi

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import { useState } from 'react';
+
+const App = ()=>{
+  const [state, setState] = useState(0);
+  const onButtonClick = ()=> {
+    setState(state + 1);
+
+  };
+
+  return(
+    <div className='App'>
+      <header className='App-header'>
+        <p>{state}</p>
+        <button onClick={onButtonClick}>click here</button>
+      </header>
+     
+    </div>
+  )
+}
+export default App;
